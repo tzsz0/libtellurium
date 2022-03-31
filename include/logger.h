0d02e751f7e3ddcf3d5b8d1611248a6dde0d99fe
tzsz0 <tim.andres@gmx.net>
2022-03-31 18:06:10 +0200

    forgot logger_t in IO decl

--- include/logger.h
+++ include/logger.h
@@ -39,22 +39,22 @@ extern logger_t *   logger_get(char const * const, logger_opts const * const);
 extern bool         logger_add_output(logger_t * const, logger_output_t const);
 
 
-extern void         logger_write(enum logger_level, char const * const, ...);
+extern void         logger_write(logger_t * const, enum logger_level, char const * const, ...);
 
 
-extern bool         logger_vwrite(enum logger_level, va_list);
+extern bool         logger_vwrite(logger_t * const, enum logger_level, va_list);
 
 
-extern void         logger_info(char const * const, ...);
+extern void         logger_info(logger_t * const, char const * const, ...);
 
 
-extern void         logger_warn(char const * const, ...);
+extern void         logger_warn(logger_t * const, char const * const, ...);
 
 
-extern void         logger_error(char const * const, ...);
+extern void         logger_error(logger_t * const, char const * const, ...);
 
 
-extern void         logger_fatal(char const * const, ...);
+extern void         logger_fatal(logger_t * const, char const * const, ...);
 
 
 extern void         logger_close(logger_t * const);
